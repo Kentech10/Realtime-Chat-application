7b3e00a51c56ea979ce3e0d8948f326f97e244eb
Kenneth Njoku <kenneth.njoku@EATNGO-AFRICA.COM>
2022-10-28 15:37:35 -0700

    first commit

---
+++ ChatCord/asgi.py
@@ -0,0 +1,23 @@
+
+
+import os
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter,  URLRouter 
+from channels.auth import AuthMiddlewareStack
+from room import routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatCord.settings')
+
+application = ProtocolTypeRouter({ 
+    
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+        
+    )}
+)
+
+
